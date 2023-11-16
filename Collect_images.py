75f68bbae205b9a62f1b44c20fa8c73730c50191
Tarunchintakunta <100082621+Tarunchintakunta@users.noreply.github.com>
2023-11-16 22:12:39 +0530

    Add files via upload

---
+++ Collect_images.py
@@ -0,0 +1,40 @@
+import os
+# pip install opencv-python
+import cv2
+
+
+#this will create a new folder called Data
+DATA_DIR = r'./data'
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+#define the no of classes you wanna create 
+number_of_classes = 5
+dataset_size = 100
+#if ur connected with other external cameras use 1 or 2,0 is for default camera 
+cap = cv2.VideoCapture(0)
+for j in range(number_of_classes):
+    if not os.path.exists(os.path.join(DATA_DIR, str(j))):
+        os.makedirs(os.path.join(DATA_DIR, str(j)))
+
+    print('Collecting dataset for class: {}'.format(j))
+
+    done = False
+    while True:
+        ret, frame = cap.read()
+        cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
+                    cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(25) == ord('q'):
+            break
+
+    counter = 0
+    while counter < dataset_size:
+        ret, frame = cap.read()
+        cv2.imshow('frame', frame)
+        cv2.waitKey(25)
+        cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
+
+        counter += 1
+
+cap.release()
+cv2.destroyAllWindows()
